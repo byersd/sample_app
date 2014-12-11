68b5f0bd4ac5c8a94291789a014cf5b59f7a3a51
byersd <byersd@gmail.com>
2014-12-11 02:46:14 +0000

    Refactored tests

--- spec/requests/static_pages_request_spec.rb
+++ spec/requests/static_pages_request_spec.rb
@@ -6,13 +6,13 @@ describe "Static Pages" do
 
   describe "Home page" do
 
+    before { visit home_path }
+
     it "should have the title 'Home'" do
-      visit '/home'
       expect(page).to have_title("Home | #{base_title}")
     end
 
     it "should have the content 'Home page'" do
-      visit '/home'
       expect(page).to have_content('Home page')
     end
 
@@ -20,13 +20,13 @@ describe "Static Pages" do
 
   describe "Help page" do
 
+    before { visit help_path }
+
     it "should have the title 'Help'" do
-      visit '/help'
       expect(page).to have_title("Help | #{base_title}")
     end
 
     it "should have the content 'Help page'" do
-      visit '/help'
       expect(page).to have_content('Help page')
     end
 
@@ -34,13 +34,13 @@ describe "Static Pages" do
 
   describe "About page" do
 
+    before { visit about_path }
+
     it "should have the title 'About'" do
-      visit '/about'
       expect(page).to have_title("About | #{base_title}")
     end
 
     it "should have the content 'About page'" do
-      visit '/about'
       expect(page).to have_content('About page')
     end
 
