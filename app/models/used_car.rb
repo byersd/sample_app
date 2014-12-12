61c78bab317f6483692a62214635eed2f08aa40b
byersd <byersd@gmail.com>
2014-12-12 04:51:30 +0000

    Added model / tests

--- app/models/used_car.rb
+++ app/models/used_car.rb
@@ -5,18 +5,22 @@ class UsedCar
   attr_accessor :car_num
   attr_accessor :car_ref
 
-  VALID_CAR_NUM_LENGTH = 23
-  VALID_CAR_REF_LENGTH = 23
-  VALID_CAR_NUM_REGEX  = /\A[A-Za-z0-9]*\z/
-  VALID_CAR_REF_REGEX  = /\A[A-Za-z0-9]*\z/
+  BASE_IMAGE_URL = "http://vcache.arnoldclark.com/imageserver/%{obfuscated_stock_reference}/%{size}/%{camera}"
+
+  MIN_CAR_NUM_LENGTH = 5
+  MIN_CAR_REF_LENGTH = 10
+  MAX_CAR_NUM_LENGTH = 10
+  MAX_CAR_REF_LENGTH = 20
+  VALID_CAR_NUM_REGEX  = /\A[A-Za-z0-9\s\-]*\z/
+  VALID_CAR_REF_REGEX  = /\A[A-Za-z0-9\s\-]*\z/
 
   validates :car_num,
     presence: true,
-    length: { maximum: VALID_CAR_NUM_LENGTH },
+    length: { minimum: MIN_CAR_NUM_LENGTH , maximum: MAX_CAR_NUM_LENGTH },
     format: { with: VALID_CAR_NUM_REGEX }
   validates :car_ref,
     presence: true,
-    length: { maximum: VALID_CAR_REF_LENGTH },
+    length: { minimum: MIN_CAR_REF_LENGTH , maximum: MAX_CAR_REF_LENGTH },
     format: { with: VALID_CAR_REF_REGEX }
 
   def initialize(attributes = {})
@@ -24,22 +28,51 @@ class UsedCar
     @car_ref = attributes[:car_ref]
   end
 
-  def display_car_num
-    get_display_car_num(@car_num)
-  end
+  def get_image_urls
 
-  def display_car_ref
-    get_display_car_ref(@car_ref)
+    image_urls = []
+
+    osr = obfuscated_stock_reference
+
+    SIZE_VALUES.each do |size|
+      CAMERA_VALUES.each do |camera|
+        image_urls << BASE_IMAGE_URL % { :obfuscated_stock_reference => osr , :size => size , :camera => camera }
+      end
+    end
+
+    return image_urls
+
   end
 
 private
 
-  def get_display_car_num(car_num)
-    "This is the car num : <#{car_num}>"
-  end
+  SIZE_VALUES = ["350",
+                 "800"]
 
-  def get_display_car_ref(car_ref)
-    "This is the car ref : <#{car_ref}>"
+  CAMERA_VALUES = ["f",
+                   "r",
+                   "i",
+                   "4",
+                   "5",
+                   "6"]
+
+  def obfuscated_stock_reference
+
+    # remove all whitespace from @car_num.
+    stripped_car_num = @car_num.gsub(/\s+/ , "")
+
+    # remove all whitespace from @car_ref.
+    stripped_car_ref = @car_ref.gsub(/\s+/ , "")
+
+    # take .reverse of stripped_car_num, and convert to char array.
+    char_array_car_num = stripped_car_num.reverse.split("")
+
+    # take [0 ... stripped_car_num.length] of stripped_car_ref, and convert to char array.
+    char_array_car_ref = stripped_car_ref[0 ... stripped_car_num.length].split("")
+
+    # interleave char arrays, flatten resulting 2D array to 1D array, combine elements to form string, append char from stripped_car_ref, and convert to uppercase.
+    char_array_car_ref.zip(char_array_car_num).flatten.join.concat(stripped_car_ref[8]).upcase
+
   end
 
 end
--- spec/models/used_car_spec.rb
+++ spec/models/used_car_spec.rb
@@ -22,14 +22,14 @@ describe UsedCar do
   describe "when car_num is not present" do
     it "should not be valid" do
       @used_car.car_num = " "
-      @used_car.car_ref = "DEF"
+      @used_car.car_ref = "ARNXY-U-123"
       should_not be_valid
     end
   end
 
   describe "when car_ref is not present" do
     it "should not be valid" do
-      @used_car.car_num = "ABC"
+      @used_car.car_num = "AB12CDE"
       @used_car.car_ref = " "
       should_not be_valid
     end
@@ -37,58 +37,106 @@ describe UsedCar do
 
   describe "when car_num and car_ref are present" do
     it "should be valid" do
-      @used_car.car_num = "ABC"
-      @used_car.car_ref = "DEF"
+      @used_car.car_num = "AB12CDE"
+      @used_car.car_ref = "ARNXY-U-123"
       should be_valid
     end
   end
 
   # Validation of attribute length.
 
+  describe "when car_num is less than minimum valid length" do
+    it "should not be valid" do
+      @used_car.car_num = "A" * (UsedCar::MIN_CAR_NUM_LENGTH - 1)
+      @used_car.car_ref = "ARNXY-U-123"
+      should_not be_valid
+    end
+  end
+
+  describe "when car_num is equal to minimum valid length" do
+    it "should be valid" do
+      @used_car.car_num = "A" * (UsedCar::MIN_CAR_NUM_LENGTH)
+      @used_car.car_ref = "ARNXY-U-123"
+      should be_valid
+    end
+  end
+
+  describe "when car_num is more than minimum valid length" do
+    it "should be valid" do
+      @used_car.car_num = "A" * (UsedCar::MIN_CAR_NUM_LENGTH + 1)
+      @used_car.car_ref = "ARNXY-U-123"
+      should be_valid
+    end
+  end
+
   describe "when car_num is less than maximum valid length" do
     it "should be valid" do
-      @used_car.car_num = "A" * (UsedCar::VALID_CAR_NUM_LENGTH - 1)
-      @used_car.car_ref = "DEF"
+      @used_car.car_num = "A" * (UsedCar::MAX_CAR_NUM_LENGTH - 1)
+      @used_car.car_ref = "ARNXY-U-123"
       should be_valid
     end
   end
 
   describe "when car_num is equal to maximum valid length" do
     it "should be valid" do
-      @used_car.car_num = "A" * (UsedCar::VALID_CAR_NUM_LENGTH)
-      @used_car.car_ref = "DEF"
+      @used_car.car_num = "A" * (UsedCar::MAX_CAR_NUM_LENGTH)
+      @used_car.car_ref = "ARNXY-U-123"
       should be_valid
     end
   end
 
   describe "when car_num is more than maximum valid length" do
     it "should not be valid" do
-      @used_car.car_num = "A" * (UsedCar::VALID_CAR_NUM_LENGTH + 1)
-      @used_car.car_ref = "DEF"
+      @used_car.car_num = "A" * (UsedCar::MAX_CAR_NUM_LENGTH + 1)
+      @used_car.car_ref = "ARNXY-U-123"
       should_not be_valid
+    end
+  end
+
+  describe "when car_ref is less than minimum valid length" do
+    it "should not be valid" do
+      @used_car.car_num = "AB12CDE"
+      @used_car.car_ref = "A" * (UsedCar::MIN_CAR_REF_LENGTH - 1)
+      should_not be_valid
+    end
+  end
+
+  describe "when car_ref is equal to minimum valid length" do
+    it "should be valid" do
+      @used_car.car_num = "AB12CDE"
+      @used_car.car_ref = "A" * (UsedCar::MIN_CAR_REF_LENGTH)
+      should be_valid
+    end
+  end
+
+  describe "when car_ref is more than minimum valid length" do
+    it "should be valid" do
+      @used_car.car_num = "AB12CDE"
+      @used_car.car_ref = "A" * (UsedCar::MIN_CAR_REF_LENGTH + 1)
+      should be_valid
     end
   end
 
   describe "when car_ref is less than maximum valid length" do
     it "should be valid" do
-      @used_car.car_num = "ABC"
-      @used_car.car_ref = "D" * (UsedCar::VALID_CAR_REF_LENGTH - 1)
+      @used_car.car_num = "AB12CDE"
+      @used_car.car_ref = "A" * (UsedCar::MAX_CAR_REF_LENGTH - 1)
       should be_valid
     end
   end
 
   describe "when car_ref is equal to maximum valid length" do
     it "should be valid" do
-      @used_car.car_num = "ABC"
-      @used_car.car_ref = "D" * (UsedCar::VALID_CAR_REF_LENGTH)
+      @used_car.car_num = "AB12CDE"
+      @used_car.car_ref = "A" * (UsedCar::MAX_CAR_REF_LENGTH)
       should be_valid
     end
   end
 
   describe "when car_ref is more than maximum valid length" do
     it "should not be valid" do
-      @used_car.car_num = "ABC"
-      @used_car.car_ref = "D" * (UsedCar::VALID_CAR_REF_LENGTH + 1)
+      @used_car.car_num = "AB12CDE"
+      @used_car.car_ref = "A" * (UsedCar::MAX_CAR_REF_LENGTH + 1)
       should_not be_valid
     end
   end
@@ -98,15 +146,15 @@ describe UsedCar do
   describe "when car_num is not of valid format" do
     it "should not be valid" do
       @used_car.car_num = "A!!"
-      @used_car.car_ref = "DEF"
+      @used_car.car_ref = "ARNXY-U-123"
       should_not be_valid
     end
   end
 
   describe "when car_ref is not of valid format" do
     it "should not be valid" do
-      @used_car.car_num = "ABC"
-      @used_car.car_ref = "D!!"
+      @used_car.car_num = "AB12CDE"
+      @used_car.car_ref = "A!!"
       should_not be_valid
     end
   end
@@ -123,26 +171,146 @@ describe UsedCar do
 
   describe "when initialized with car_num only" do
     it "should have car_num only" do
-      initialized_used_car = UsedCar.new(car_num: "ABC")
-      expect(initialized_used_car.car_num).to eq("ABC")
+      initialized_used_car = UsedCar.new(car_num: "AB12CDE")
+      expect(initialized_used_car.car_num).to eq("AB12CDE")
       expect(initialized_used_car.car_ref).to be_nil
     end
   end
 
   describe "when initialized with car_ref only" do
     it "should have car_ref only" do
-      initialized_used_car = UsedCar.new(car_ref: "DEF")
+      initialized_used_car = UsedCar.new(car_ref: "ARNXY-U-123")
       expect(initialized_used_car.car_num).to be_nil
-      expect(initialized_used_car.car_ref).to eq("DEF")
+      expect(initialized_used_car.car_ref).to eq("ARNXY-U-123")
     end
   end
 
   describe "when initialized with car_num and car_ref" do
     it "should have car_num and car_ref" do
-      initialized_used_car = UsedCar.new(car_num: "ABC" , car_ref: "DEF")
-      expect(initialized_used_car.car_num).to eq("ABC")
-      expect(initialized_used_car.car_ref).to eq("DEF")
+      initialized_used_car = UsedCar.new(car_num: "AB12CDE" , car_ref: "ARNXY-U-123")
+      expect(initialized_used_car.car_num).to eq("AB12CDE")
+      expect(initialized_used_car.car_ref).to eq("ARNXY-U-123")
     end
+  end
+
+  # Tests for get_image_urls.
+
+  describe "when car_num is of length 7" do
+    it "should have correct image urls" do
+      expected_image_urls = [UsedCar::BASE_IMAGE_URL % { :obfuscated_stock_reference => "AERDNCX2Y1-BUA3" , :size => "350" , :camera => "f" },
+                             UsedCar::BASE_IMAGE_URL % { :obfuscated_stock_reference => "AERDNCX2Y1-BUA3" , :size => "350" , :camera => "r" },
+                             UsedCar::BASE_IMAGE_URL % { :obfuscated_stock_reference => "AERDNCX2Y1-BUA3" , :size => "350" , :camera => "i" },
+                             UsedCar::BASE_IMAGE_URL % { :obfuscated_stock_reference => "AERDNCX2Y1-BUA3" , :size => "350" , :camera => "4" },
+                             UsedCar::BASE_IMAGE_URL % { :obfuscated_stock_reference => "AERDNCX2Y1-BUA3" , :size => "350" , :camera => "5" },
+                             UsedCar::BASE_IMAGE_URL % { :obfuscated_stock_reference => "AERDNCX2Y1-BUA3" , :size => "350" , :camera => "6" },
+                             UsedCar::BASE_IMAGE_URL % { :obfuscated_stock_reference => "AERDNCX2Y1-BUA3" , :size => "800" , :camera => "f" },
+                             UsedCar::BASE_IMAGE_URL % { :obfuscated_stock_reference => "AERDNCX2Y1-BUA3" , :size => "800" , :camera => "r" },
+                             UsedCar::BASE_IMAGE_URL % { :obfuscated_stock_reference => "AERDNCX2Y1-BUA3" , :size => "800" , :camera => "i" },
+                             UsedCar::BASE_IMAGE_URL % { :obfuscated_stock_reference => "AERDNCX2Y1-BUA3" , :size => "800" , :camera => "4" },
+                             UsedCar::BASE_IMAGE_URL % { :obfuscated_stock_reference => "AERDNCX2Y1-BUA3" , :size => "800" , :camera => "5" },
+                             UsedCar::BASE_IMAGE_URL % { :obfuscated_stock_reference => "AERDNCX2Y1-BUA3" , :size => "800" , :camera => "6" }]
+
+      @used_car.car_num = " AB12 CDE "
+      @used_car.car_ref = " ARNXY-U-34567 "
+      should be_valid
+      expect(@used_car.get_image_urls).to eq(expected_image_urls)
+    end
+  end
+
+  describe "when car_num is of length 8" do
+    it "should have correct image urls" do
+      expected_image_urls = [UsedCar::BASE_IMAGE_URL % { :obfuscated_stock_reference => "AFRENDXCY2-1UB-A4" , :size => "350" , :camera => "f" },
+                             UsedCar::BASE_IMAGE_URL % { :obfuscated_stock_reference => "AFRENDXCY2-1UB-A4" , :size => "350" , :camera => "r" },
+                             UsedCar::BASE_IMAGE_URL % { :obfuscated_stock_reference => "AFRENDXCY2-1UB-A4" , :size => "350" , :camera => "i" },
+                             UsedCar::BASE_IMAGE_URL % { :obfuscated_stock_reference => "AFRENDXCY2-1UB-A4" , :size => "350" , :camera => "4" },
+                             UsedCar::BASE_IMAGE_URL % { :obfuscated_stock_reference => "AFRENDXCY2-1UB-A4" , :size => "350" , :camera => "5" },
+                             UsedCar::BASE_IMAGE_URL % { :obfuscated_stock_reference => "AFRENDXCY2-1UB-A4" , :size => "350" , :camera => "6" },
+                             UsedCar::BASE_IMAGE_URL % { :obfuscated_stock_reference => "AFRENDXCY2-1UB-A4" , :size => "800" , :camera => "f" },
+                             UsedCar::BASE_IMAGE_URL % { :obfuscated_stock_reference => "AFRENDXCY2-1UB-A4" , :size => "800" , :camera => "r" },
+                             UsedCar::BASE_IMAGE_URL % { :obfuscated_stock_reference => "AFRENDXCY2-1UB-A4" , :size => "800" , :camera => "i" },
+                             UsedCar::BASE_IMAGE_URL % { :obfuscated_stock_reference => "AFRENDXCY2-1UB-A4" , :size => "800" , :camera => "4" },
+                             UsedCar::BASE_IMAGE_URL % { :obfuscated_stock_reference => "AFRENDXCY2-1UB-A4" , :size => "800" , :camera => "5" },
+                             UsedCar::BASE_IMAGE_URL % { :obfuscated_stock_reference => "AFRENDXCY2-1UB-A4" , :size => "800" , :camera => "6" }]
+
+      @used_car.car_num = " AB12CDEF "
+      @used_car.car_ref = " ARNXY-U-45678 "
+      should be_valid
+      expect(@used_car.get_image_urls).to eq(expected_image_urls)
+    end
+  end
+
+  # Tests for obfuscated_stock_reference.
+
+  describe "when car_num is of length 5" do
+
+    it "should have correct obfuscated_stock_reference" do
+      @used_car.car_num = " AB 12 C "
+      @used_car.car_ref = " ARNXY-U-12345 "
+      should be_valid
+      expect(@used_car.send(:obfuscated_stock_reference)).to eq("ACR2N1XBYA1")
+    end
+
+    it "should, regardless of case, have correct obfuscated_stock_reference" do
+      @used_car.car_num = " ab 12 C "
+      @used_car.car_ref = " aRnXy-U-12345 "
+      should be_valid
+      expect(@used_car.send(:obfuscated_stock_reference)).to eq("ACR2N1XBYA1")
+    end
+
+  end
+
+  describe "when car_num is of length 6" do
+
+    it "should have correct obfuscated_stock_reference" do
+      @used_car.car_num = " AB 12 CD "
+      @used_car.car_ref = " ARNXY-U-23456 "
+      should be_valid
+      expect(@used_car.send(:obfuscated_stock_reference)).to eq("ADRCN2X1YB-A2")
+    end
+
+    it "should, regardless of case, have correct obfuscated_stock_reference" do
+      @used_car.car_num = " ab 12 CD "
+      @used_car.car_ref = " aRnXy-U-23456 "
+      should be_valid
+      expect(@used_car.send(:obfuscated_stock_reference)).to eq("ADRCN2X1YB-A2")
+    end
+
+  end
+
+  describe "when car_num is of length 7" do
+
+    it "should have correct obfuscated_stock_reference" do
+      @used_car.car_num = " AB12 CDE "
+      @used_car.car_ref = " ARNXY-U-34567 "
+      should be_valid
+      expect(@used_car.send(:obfuscated_stock_reference)).to eq("AERDNCX2Y1-BUA3")
+    end
+
+    it "should, regardless of case, have correct obfuscated_stock_reference" do
+      @used_car.car_num = " ab12 CDE "
+      @used_car.car_ref = " aRnXy-U-34567 "
+      should be_valid
+      expect(@used_car.send(:obfuscated_stock_reference)).to eq("AERDNCX2Y1-BUA3")
+    end
+
+  end
+
+  describe "when car_num is of length 8" do
+
+    it "should have correct obfuscated_stock_reference" do
+      @used_car.car_num = " AB12CDEF "
+      @used_car.car_ref = " ARNXY-U-45678 "
+      should be_valid
+      expect(@used_car.send(:obfuscated_stock_reference)).to eq("AFRENDXCY2-1UB-A4")
+    end
+
+    it "should, regardless of case, have correct obfuscated_stock_reference" do
+      @used_car.car_num = " ab12CDEF "
+      @used_car.car_ref = " aRnXy-U-45678 "
+      should be_valid
+      expect(@used_car.send(:obfuscated_stock_reference)).to eq("AFRENDXCY2-1UB-A4")
+    end
+
   end
 
 end
